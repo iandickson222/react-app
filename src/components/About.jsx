7b77eb0eb0c2859da38b2c446920015252b2158e
iandickson222 <53797442+iandickson222@users.noreply.github.com>
2020-05-13 22:25:41 -0700

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,8 @@ const About = () => {
                 <h2>About Me</h2>
                 <p> 
                     My name is Ian Dickson. 
-                    I am an engineer, a developer, a graphic designer, and much more. 
-                    I enjoy taking on my own personal projects when I am not helping others to complete theirs. 
-                    This site is a side project of mine to help me develop my web skills.
+                    I am an engineer, a developer, a graphic designer, and much more.
+                    I developed this site as a way to display my web development skills. 
                     Also, make sure to check out my new game Cosmic Spikes! 
                 </p>
             </div>
